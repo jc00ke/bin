d650190b45f44c95eba45fb8ae9f5ae9c90527a4
jc00ke <jesse@jc00ke.com>
2015-08-26 14:54:59 -0700

    Fake out Rubocop if .rubocop.yml is absent

--- rubocop.sh
+++ rubocop.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 if [ $# -eq 1 -a x"$1" = x--version ]; then
-    rubocop "$@" 2>&1 | sed '/^warning:/ d'
+  rubocop "$@" 2>&1 | sed '/^warning:/ d'
+elif [ ! -f .rubocop.yml ]; then
+    return 0
 else
-    exec rubocop "$@"
+  exec rubocop "$@"
 fi
